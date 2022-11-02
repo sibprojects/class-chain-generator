6f4baebceef9e94a3f1c5d0d2f8cd6647badb814
Siberian <siberian@sibprojects.com>
2022-11-02 23:08:25 +0000

    update

---
+++ src/Factory/StatusChain/Helper/PropertyHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Factory\StatusChain\Helper;
+
+class PropertyHelper
+{
+    public string $name;
+    public string $nameUc;
+    public ?string $typeName;
+    public string $nullModifier = '';
+}
