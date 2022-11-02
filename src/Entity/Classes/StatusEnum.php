05143d9605c9b631a0c1d347ab879d9843a52645
Siberian <siberian@sibprojects.com>
2022-11-02 23:08:54 +0000

    update

---
+++ src/Entity/Classes/StatusEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Entity\Classes;
+
+class StatusEnum extends BaseEnum
+{
+    public const PENDING = 0;
+    public const COMPLETED = 1;
+
+    public static function getDefault(): int
+    {
+        return self::PENDING;
+    }
+
+    public static function getDefaultName(): string
+    {
+        return parent::getNameById(self::PENDING);
+    }
+}
